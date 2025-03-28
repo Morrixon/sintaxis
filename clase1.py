de29e163f84a2dc931144dbc6641ae7940aae03d
VMP <morrisoneano@gmail.com>
2025-03-28 19:38:10 -0300

    Agrego texto en clase 1

--- clase1.py
+++ clase1.py
@@ -1,1 +1,14 @@
 #TAD (TIPO ABSTRACTO DE DATO)
+#TADEstudiante
+
+#est=[Nombre,Apellido,Legajo,Promedio]
+#est=[","0,0]
+
+#def crearEstudiante():
+#crea un estudiante vacio
+
+#def cargarEstudiante(Estudiante,nom,ape,leg,prom);
+#inicializa estudiante con sus datos
+
+#def asignarEstudiante(estudiante1,estudiante2);
+#Asigna los datos del estudiante1 al estudiante2(copia al estudiante)
