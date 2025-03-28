ce39bf4459a23bd79b3727e28f1035dcbb090679
VMP <morrisoneano@gmail.com>
2025-03-28 19:46:11 -0300

    Sumo el ejercicio

--- clase1.py
+++ clase1.py
@@ -12,3 +12,10 @@
 
 #def asignarEstudiante(estudiante1,estudiante2);
 #Asigna los datos del estudiante1 al estudiante2(copia al estudiante)
+
+#------------EJERCICIO 1 DE CLASE -------------------
+
+#Crear dos estudiante y cargarlos
+#Imprimir los datos de cada estudiante al finalizar su respectiva carga
+#Comparar los promedios e imprimir un cartel que diga que estudiante mayor promedio
+
