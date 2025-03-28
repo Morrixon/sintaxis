65b2e957be78259819fd4dcccb03a49aed719502
VMP <morrisoneano@gmail.com>
2025-03-28 20:23:32 -0300

    arme los tipos de datos tad

--- clase1.py
+++ clase1.py
@@ -1,8 +1,8 @@
 #TAD (TIPO ABSTRACTO DE DATO)
 #TADEstudiante
 
-#est=[Nombre,Apellido,Legajo,Promedio]
-#est=[","0,0]
+est=[Nombre,Apellido,Legajo,Promedio]
+est=[" ",0,0]
 
 #def crearEstudiante():
 #crea un estudiante vacio
@@ -18,4 +18,37 @@
 #Crear dos estudiante y cargarlos
 #Imprimir los datos de cada estudiante al finalizar su respectiva carga
 #Comparar los promedios e imprimir un cartel que diga que estudiante mayor promedio
+
+#TAD
+
+""" nom = "Chill"
+print(nom)
+
+apellido= input("Ingresa tu apellido de chill \n")
+
+print(apellido)
+
+numero= int(input("Ingrese un numero \n"))
+print(numero) """
+
+
+import TADEstudiante
+
+from TADestudiante import *
+
+est1= crearEstudiante():
+est2=crearEstudiante():
+nombre= input("ingrese el nombre \n")
+apellido= input("ingrese apellido \n")
+legajo=int(input("Ingrese el nro de legajo \n"))
+nota=float(input("Ingrese la nota del alumno \n"))
+
+cargarEst(est1,nombre,apellido,legajo,nota)
+
+nombre= input("ingrese el nombre \n")
+apellido= input("ingrese apellido \n")
+legajo=int(input("Ingrese el nro de legajo \n"))
+nota=float(input("Ingrese la nota del alumno \n"))
+
+cargarEst(est2,nombre,apellido,legajo,nota)
 
